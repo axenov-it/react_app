fcc535b6739142781adfd1c554455b506d9780c0
alex0735070005 <alex0735070005@gmail.com>
2021-03-14 13:09:06 +0200

    dev menu items

--- src/components/Header/components/Menu/index.jsx
+++ src/components/Header/components/Menu/index.jsx
@@ -1,35 +1,15 @@
 import "./styles.css";
 
-function Menu() {
-  return (
-    <ul className="menu">
-      <li className="menu__item">
-        <a className="menu__link" href="#">
-          Home
-        </a>
-      </li>
-      <li className="menu__item">
-        <a className="menu__link" href="#">
-          About us
-        </a>
-      </li>
-      <li className="menu__item">
-        <a className="menu__link" href="#">
-          Gallery
-        </a>
-      </li>
-      <li className="menu__item">
-        <a className="menu__link" href="#">
-          Contacts
-        </a>
-      </li>
-      <li className="menu__item">
-        <a className="menu__link" href="#">
-          Catalog
-        </a>
-      </li>
-    </ul>
-  );
+function Menu(props) {
+  const htmlItems = props.items.map((item) => (
+    <li key={item.id} className="menu__item">
+      <a className="menu__link" href={item.link}>
+        {item.title}
+      </a>
+    </li>
+  ));
+
+  return <ul className="menu">{htmlItems}</ul>;
 }
 
 export default Menu;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,13 @@
 import Menu from "./components/Menu";
+import menuItems from "system/menu.json";
+
 import "./styles.css";
 
 function Header() {
   return (
     <header className="header">
       <div className="header__wrap">
-        <Menu />
+        <Menu items={menuItems} type="vertical" />
       </div>
     </header>
   );
